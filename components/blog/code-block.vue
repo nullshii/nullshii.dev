144ac54b99c0329d5766643803db49e3c6a0cda2
nullshii <bruh@bruh.com>
2023-11-05 01:58:20 +0300

    Added bit more to ssh config blog

--- components/blog/code-block.vue
+++ components/blog/code-block.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-
+	<div class='bg-secondary rounded-xl p-2'>
+		<slot />
+	</div>
 </template>
 
 <style scoped>
--- pages/blog/github-ssh-config.vue
+++ pages/blog/github-ssh-config.vue
@@ -1,5 +1,6 @@
 <script setup lang='ts'>
 	import '~/assets/css/blogs.css';
+	import CodeBlock from '~/components/blog/code-block.vue';
 </script>
 
 <template>
@@ -15,13 +16,29 @@
 				</p>
 			</div>
 			<div class='disclaimer-container'>
-				{{ $t('blog.github-ssh-config.disclaimer.os') }}
+				{{ $t('blog.github-ssh-config.warning.os') }}
 			</div>
-			<ol>
-				<li>
-					{{ $t('blog.github-ssh-config.create-necessary-files') }}
-				</li>
-			</ol>
+			<div class='note-container'>
+				<div class='note'>
+					{{ $t('blog.note') }}
+				</div>
+				<p v-for="(item, index) in $t('blog.github-ssh-config.note.home-folder-location').split('\n')"
+					 :key='index'>
+					{{ item }}
+				</p>
+			</div>
+			{{ $t('blog.github-ssh-config.create-necessary-files') }}
+			<code-block>
+				<p>
+					$HOME/.ssh/ <span class='text-accent'>- {{ $t('blog.github-ssh-config.folder.base') }}</span>
+				</p>
+				<p>
+					$HOME/.ssh/keys <span class='text-accent'>- {{ $t('blog.github-ssh-config.folder.keys') }}</span>
+				</p>
+				<p>
+					$HOME/.ssh/config <span class='text-accent'>- {{ $t('blog.github-ssh-config.file.config') }}</span>
+				</p>
+			</code-block>
 		</center-container>
 		<blog-back-to-index />
 	</div>
